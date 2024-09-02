b1a137282223de7899cd5404a8c77d151572ffaf
Lucas Machado <lucasmachadoale4@gmail.com>
2024-09-02 20:05:18 -0300

    feature: created the Colaborador folder

---
+++ Colaborador/src/Colaborador.java
@@ -0,0 +1,17 @@
+public class Colaborador {
+    private double provento;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome() {
+        this.nome = nome;
+    }
+    public void setProvento() {
+        this.provento = provento;
+    }
+    public double getProvento() {
+
+    }
+}
