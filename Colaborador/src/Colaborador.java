13c9df668f81188048527bb1cc8342190aefe3e8
Lucas Machado <lucasmachadoale4@gmail.com>
2024-09-02 21:58:45 -0300

    feature: created comented descriptions

--- Colaborador/src/Colaborador.java
+++ Colaborador/src/Colaborador.java
@@ -2,16 +2,16 @@ public class Colaborador {
     private double provento;
     private String nome;
 
-    public String getNome() {
+    public String getNome() { // aplicando o método get para nome
         return nome;
     }
-    public void setNome(String nome) {
+    public void setNome(String nome) { // aplicando o método set para nome
         this.nome = nome;
     }
-    public void setProvento(double provento) {
+    public void setProvento(double provento) { // aplicando o método set para provento
         this.provento = provento;
     }
-    public double getProvento() {
+    public double getProvento() { // aplicando o método get para provento
         return provento;
     }
 }
--- Colaborador/src/procColaborador.java
+++ Colaborador/src/procColaborador.java
@@ -2,10 +2,11 @@ public class procColaborador {
     public static void main (String[] args) {
         Colaborador colab = new Colaborador();
 
-
+        //usando o método set para redefinir os valores de nome e provento
         colab.setNome("Stanford Phines");
         colab.setProvento(5700);
 
+        //usando o método get para receber os valores de nome e provento
         System.out.println(colab.getNome());
         System.out.println(colab.getProvento());
     }
